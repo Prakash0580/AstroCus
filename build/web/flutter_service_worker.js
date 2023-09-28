166662e685390e1fae1ff174a181cb70b6378036
Prakash Kumar <prakeshkumar0580@gmail.com>
2023-09-28 16:49:16 +0530

    Build Updated

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -24,9 +24,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "db55d7790bc5bc1e3aec9669a15a6e3e
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "7f3c90c8c47eea7c97082e254ec881a8",
-"/": "7f3c90c8c47eea7c97082e254ec881a8",
-"main.dart.js": "0d7806ff7d2ada19917d85d96b17a110",
+"index.html": "caf6f1f7dd8e14de1c0f751ce6237729",
+"/": "caf6f1f7dd8e14de1c0f751ce6237729",
+"main.dart.js": "c091203c09af48ef2edeb23ac90c79e9",
 "manifest.json": "e7dc17d1a4055596094f11356d5d37e6",
 "version.json": "369074379aef8296e58626e0de2806e2"};
 // The application shell files that are downloaded before a service worker can
--- lib/mobile/mobile.dart
+++ lib/mobile/mobile.dart
@@ -5,6 +5,13 @@ class MobileScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("AstroKapish"),
+      ),
+      body: Center(
+        child: Text("Under Construction............"),
+      ),
+    );
   }
 }
